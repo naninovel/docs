c3c8453916ba33edde9402c6d6aceef5afd64fa7
Artyom Sovetnikov <elringus@yandex.ru>
2019-01-10 13:46:58 +0300

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,8 @@ module.exports = {
         ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
         ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
         ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+        ['meta', { name: 'google-site-verification', content: 'cdvgJ2XFFbaGErDKJtTbFj9u9frfReZ9rzUnsf9F6nI' }]
     ],
     themeConfig: {
         editLinks: false,
