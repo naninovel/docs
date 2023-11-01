6facb816fc13003ab4c78a5947ab23de9e6aeb07
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-11-01 22:23:17 +0300

    fix video

--- docs/.vitepress/ext/vite/vite.js
+++ docs/.vitepress/ext/vite/vite.js
@@ -4,7 +4,15 @@ import { EmbedAssets } from "./vite-embed";
 /** @type import("vite").UserConfig */
 export const Vite = {
     resolve: { alias: [override("NotFound", "not-found")] },
-    plugins: [EmbedAssets({ assetsDir: "./docs/.vitepress/cache/.remote-assets" })]
+    plugins: [
+        EmbedAssets({ assetsDir: "./docs/.vitepress/cache/.remote-assets" }),
+        // A hack due to vite not resolving "data-source" attr: https://github.com/vitejs/vite/issues/5098
+        {
+            name: "data-source",
+            transformIndexHtml: (html) => html.replace(
+                new RegExp(/<source data-src="(.+?)"/g), "<source data-src=\"$1\" src=\"$1\"")
+        }
+    ]
 };
 
 /** https://vitepress.dev/guide/extending-default-theme#overriding-internal-components
