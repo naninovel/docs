8bfcf4483dc9e0472ddda2a8c6c43bf394dbc54d
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-11-01 21:28:26 +0300

    lazy load media

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,5 +3,39 @@ import "@fontsource-variable/jetbrains-mono";
 import "./style.css";
 
 // https://vitepress.dev/guide/extending-default-theme
+// noinspection JSUnusedGlobalSymbols
 
-export default { extends: DefaultTheme };
+export default {
+    extends: {
+        Layout: DefaultTheme.Layout,
+        /** @param {import("vitepress").EnhanceAppContext} ctx */
+        enhanceApp: (ctx) => {
+            DefaultTheme.enhanceApp(ctx);
+            ctx.router.onAfterRouteChanged = watchVideos;
+        }
+    }
+};
+
+function watchVideos() {
+    if (typeof document !== "object") return;
+    const videos = document.querySelectorAll("video.video");
+    const observer = new IntersectionObserver(handleIntersections);
+    for (const video of videos)
+        observer.observe(video);
+}
+
+function handleIntersections(videos, observer) {
+    for (const video of videos)
+        handleIntersection(video, observer);
+}
+
+function handleIntersection(video, observer) {
+    if (!video.isIntersecting) return;
+    for (const id in video.target.children) {
+        const source = video.target.children[id];
+        if (typeof source.tagName === "string" && source.tagName === "SOURCE")
+            source.src = source.dataset.src;
+    }
+    video.target.load();
+    observer.unobserve(video.target);
+}
