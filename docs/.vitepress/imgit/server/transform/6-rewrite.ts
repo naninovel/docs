59a84e12a191757a724903346ad41c5ec626d56c
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-11-09 22:23:01 +0300

    etc

--- docs/.vitepress/imgit/server/transform/6-rewrite.ts
+++ docs/.vitepress/imgit/server/transform/6-rewrite.ts
@@ -3,6 +3,7 @@ import { BuiltAsset } from "../asset";
 /** Rewrites content of the document with specified assets; returns modified document content. */
 export async function rewrite(content: string, assets: BuiltAsset[]): Promise<string> {
     const replaced = new Set<string>;
+    // TODO: How to replace all at once?
     for (const asset of assets)
         replace(asset.syntax, asset.html);
     return content;
