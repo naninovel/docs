12926ac5288a9cdf19f02a4199b9b383231e4081
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-11-12 21:09:28 +0300

    add alpha probe

--- docs/.vitepress/imgit/server/transform/3-probe.ts
+++ docs/.vitepress/imgit/server/transform/3-probe.ts
@@ -31,7 +31,67 @@ async function probeMedia(path: string, url: string): Promise<MediaInfo> {
 }
 
 function parseOut(out: string): MediaInfo {
-    if (!out?.includes("x")) return { width: NaN, height: NaN, alpha: false };
-    const parts = out.split("x");
-    return { width: Number(parts[0]), height: Number(parts[1]), alpha: false };
+    if (!out?.includes(",")) return { width: NaN, height: NaN, alpha: false };
+    const parts = out.split(",");
+    const alpha = alphaFormats.has(parts[2].trim());
+    console.log(parts[2]);
+    return { width: Number(parts[0]), height: Number(parts[1]), alpha };
 }
+
+// Print list of all the known formats with alpha channel (may vary with ffprobe version):
+// ffprobe -show_entries pixel_format=name:flags=alpha -of csv=p=0
+const alphaFormats = new Set<string>([
+    "pal8",
+    "argb",
+    "rgba",
+    "abgr",
+    "bgra",
+    "yuva420p",
+    "ya8",
+    "yuva422p",
+    "yuva444p",
+    "yuva420p9be",
+    "yuva420p9le",
+    "yuva422p9be",
+    "yuva422p9le",
+    "yuva444p9be",
+    "yuva444p9le",
+    "yuva420p10be",
+    "yuva420p10le",
+    "yuva422p10be",
+    "yuva422p10le",
+    "yuva444p10be",
+    "yuva444p10le",
+    "yuva420p16be",
+    "yuva420p16le",
+    "yuva422p16be",
+    "yuva422p16le",
+    "yuva444p16be",
+    "yuva444p16le",
+    "rgba64be",
+    "rgba64le",
+    "bgra64be",
+    "bgra64le",
+    "ya16be",
+    "ya16le",
+    "gbrap",
+    "gbrap16be",
+    "gbrap16le",
+    "ayuv64le",
+    "ayuv64be",
+    "gbrap12be",
+    "gbrap12le",
+    "gbrap10be",
+    "gbrap10le",
+    "gbrapf32be",
+    "gbrapf32le",
+    "yuva422p12be",
+    "yuva422p12le",
+    "yuva444p12be",
+    "yuva444p12le",
+    "vuya",
+    "rgbaf16be",
+    "rgbaf16le",
+    "rgbaf32be",
+    "rgbaf32le"
+]);
