4eab49bd14912262e6c3d42708c64d26d52f4040
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-11-18 22:09:09 +0300

    iteration

--- docs/.vitepress/imgit/server/encoder/ffmpeg.ts
+++ docs/.vitepress/imgit/server/encoder/ffmpeg.ts
@@ -3,34 +3,40 @@ import { config } from "../config";
 import { getExtension } from "../common";
 import { EncodeProps } from "./encoder";
 
-const stillXts = new Set<string>(config.image);
-const videoXts = new Set<string>(config.video);
-
 export async function ffmpeg(props: EncodeProps): Promise<void> {
-    const xt = getExtension(props.input);
-    const cmd = `ffmpeg ${buildArgs()}`;
-    const { err } = await platform.exec(cmd);
+    const { err } = await platform.exec(`ffmpeg ${buildArgs()}`);
     if (err) config.log?.err?.(`ffmpeg error: ${err}`);
 
     function buildArgs(): string {
         // https://trac.ffmpeg.org/wiki/Encode/AV1
-        const codec = stillXts.has(xt) ? "-still-picture 1" : videoXts.has(xt) ? "-c:v libaom-av1" : null;
+        const still = isImage(props.input) ? "-still-picture 1" : null;
+        const codec = isVideo(props.input) ? "-c:v libaom-av1" : null;
         const quality = `-crf ${lerp(63, 23, props.quality)} -cpu-used ${props.speed * 10}`;
         const filter = buildFilter();
         const map = props.probe.alpha ? `-map "[rgb]" -map "[a]"` : `-map "[rgb]"`;
-        const options = ["-loglevel error", "-y", codec, quality, filter, map].filter(x => x).join(" ");
+        const options = ["-loglevel error", "-y", still, codec, quality, filter, map].filter(x => x).join(" ");
         return `-i "${(props.input)}" ${options} "${(props.output)}"`;
     }
 
     function buildFilter(): string | undefined {
         // https://trac.ffmpeg.org/wiki/FilteringGuide
-        const select = !stillXts.has(xt) && props.single ? "select=eq(n\\,0)" : null;
-        const scale = props.width ? `scale=${(props.width)}:-2` : "copy";
+        const select = !isImage(props.input) && props.single ? "select=eq(n\\,0)" : null;
+        const scale = props.width ? `scale=${(props.width)}:-1` : "copy";
         const box = props.blur ? `boxblur=${props.blur * 10}` : null;
         const rgb = [select, scale, box].filter(x => x).join(",");
         const a = [select, "alphaextract", scale, box].filter(x => x).join(",");
         return `-filter_complex "[0:v]${rgb}[rgb];${props.probe.alpha ? `[0:v]${a}[a]` : ""}"`;
     }
+}
+
+function isImage(path: string) {
+    const xt = getExtension(path);
+    return config.image.includes(xt);
+}
+
+function isVideo(path: string) {
+    const xt = getExtension(path);
+    return config.video.includes(xt);
 }
 
 function lerp(a: number, b: number, w: number) {
