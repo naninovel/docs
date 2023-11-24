cc30eacc158413bc8e2d8feb64335e634f39f872
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-11-25 00:13:25 +0300

    etc

--- docs/.vitepress/imgit/server/transform/index.ts
+++ docs/.vitepress/imgit/server/transform/index.ts
@@ -4,7 +4,7 @@ import { config } from "../config";
 /** Transforms source document (eg, <code>.md</code>, <code>.jsx</code> or <code>.html</code>)
  *  with specified content replacing configured asset syntax with optimized HTML.
  *  @param content Text content of the document to transform.
- *  @param ctx Shared state of the current server run.
+ *  @param ctx Shared state of the current build operation.
  *  @return Transformed content of the document. */
 export async function transform(content: string, ctx: Context): Promise<string> {
     const captured = await config.transform.capture(content, ctx);
