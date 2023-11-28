778d925504b8624db80624085085f35edd0ef1a7
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-11-28 22:03:47 +0300

    iteration

--- docs/.vitepress/imgit/server/transform/6-build.ts
+++ docs/.vitepress/imgit/server/transform/6-build.ts
@@ -10,10 +10,10 @@ export function build(assets: EncodedAsset[]): Promise<BuiltAsset[]> {
 
 async function buildAsset(asset: EncodedAsset): Promise<BuiltAsset> {
     let html: string;
-    if (asset.type === AssetType.Image) html = await cfg.build.image(asset);
-    else if (asset.type === AssetType.Animation) html = await cfg.build.animation(asset);
-    else if (asset.type === AssetType.Video) html = await cfg.build.video(asset);
-    else html = await cfg.build.youtube(asset);
+    if (asset.type === AssetType.Image) html = await cfg.builders.image(asset);
+    else if (asset.type === AssetType.Animation) html = await cfg.builders.animation(asset);
+    else if (asset.type === AssetType.Video) html = await cfg.builders.video(asset);
+    else html = await cfg.builders.youtube(asset);
     return { ...asset, html };
 }
 
@@ -27,12 +27,12 @@ export function buildAnimation(asset: EncodedAsset): Promise<string> {
 
 export async function buildVideo(asset: EncodedAsset): Promise<string> {
     const { src, encodedSrc } = await buildSources(asset);
-    const cls = `class="${cfg.build.style.className.video}"`;
+    const cls = `class="${cfg.builders.style.className.video}"`;
     const size = buildSizes(asset.content);
     // https://jakearchibald.com/2022/html-codecs-parameter-for-av1
     const codec = "av01.0.04M.08"; // TODO: Resolve for each file in probe.
     return `
-<div class="${cfg.build.style.className.container}">
+<div class="${cfg.builders.style.className.container}">
     <video data-imgit preload="none" loop autoplay muted playsinline ${cls} ${size}>
         ${encodedSrc ? `<source data-src="${encodedSrc}" type="video/mp4; codecs=${codec}">` : ""}
         <source data-src="${src}" type="video/mp4">
@@ -42,7 +42,7 @@ export async function buildVideo(asset: EncodedAsset): Promise<string> {
 }
 
 export async function buildYouTube(asset: EncodedAsset): Promise<string> {
-    const cls = `class="${cfg.build.style.className.youtube}"`;
+    const cls = `class="${cfg.builders.style.className.youtube}"`;
     const id = asset.sourceUrl.split("youtube.com/watch?v=")[1];
     const source = `https://www.youtube-nocookie.com/embed/${id}`;
     return `
@@ -55,13 +55,13 @@ async function buildPicture(asset: EncodedAsset): Promise<string> {
     const { src, encodedSrc, encoded2xSrc } = await buildSources(asset);
     const size = buildSizes(asset.content);
     const cls = asset.type === AssetType.Image
-        ? cfg.build.style.className.image
-        : cfg.build.style.className.animation;
+        ? cfg.builders.style.className.image
+        : cfg.builders.style.className.animation;
     const x2 = encoded2xSrc ? `, ${encoded2xSrc} 2x` : "";
     const lazy = asset.spec?.lazy !== false;
     const load = lazy ? `loading="lazy" decoding="async"` : `decoding="sync"`;
     return `
-<div class="${cfg.build.style.className.container}">
+<div class="${cfg.builders.style.className.container}">
     <picture>
         ${encodedSrc ? `<source srcset="${encodedSrc} 1x${x2}" type="image/avif"/>` : ""}
         <img data-imgit src="${src}" alt="${asset.alt}" class="${cls}" ${size} ${load}/>
@@ -81,8 +81,8 @@ async function buildSources(asset: EncodedAsset) {
 function serve(fullPath: string, asset: EncodedAsset): string | Promise<string> {
     const fullRoot = std.path.resolve(cfg.root);
     const relativePath = fullPath.substring(fullRoot.length + 1);
-    if (cfg.serve === "auto") return `${cfg.host}/${relativePath}`;
-    return cfg.serve(relativePath, asset);
+    if (cfg.servers === "auto") return `${cfg.host}/${relativePath}`;
+    return cfg.servers(relativePath, asset);
 }
 
 function buildSizes(info?: AssetContent): string {
@@ -95,7 +95,7 @@ function buildSizes(info?: AssetContent): string {
 
 async function buildPoster(asset: EncodedAsset, size: string): Promise<string> {
     if (!cfg.poster) return "";
-    const cls = cfg.build.style.className.poster;
+    const cls = cfg.builders.style.className.poster;
     const avif = asset.encodedPosterPath
         ? await getPosterBase64(asset.sourceUrl, asset.encodedPosterPath, asset.dirty)
         : undefined;
