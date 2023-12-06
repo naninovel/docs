ab0b775b7ca79dbd79dccf41be607a34ad2a4322
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-06 19:18:39 +0300

    iteration

--- docs/.vitepress/imgit/plugin/youtube.ts
+++ docs/.vitepress/imgit/plugin/youtube.ts
@@ -9,6 +9,11 @@ type YouTubeCache = Cache & {
 
 /** YouTube thumbnail variants; each video is supposed to have at least "0". */
 const thumbs = ["maxresdefault", "mqdefault", "0"];
+const playButtonSvg = `
+<svg height="100%" viewBox="0 0 68 48" width="100%" class="imgit-youtube-play-button">
+    <path fill="#f00" d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"></path>
+    <path fill="#fff" d="M 45,24 27,14 27,34"></path>
+</svg>`;
 
 /** Allows embedding YouTube videos with imgit.
  *  @example ![](https://www.youtube.com/watch?v=J7UwSVsiwzI) */
