f416015fa36e6e5c6cbe254ba857a9315b2c8a6f
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-09 03:19:39 +0300

    iteration

--- docs/.vitepress/imgit/plugin/youtube/client.ts
+++ docs/.vitepress/imgit/plugin/youtube/client.ts
@@ -1,1 +1,42 @@
+import { addHandler } from "../../client/mutation";
 import "./styles.css";
+
+addHandler([handleAdded, handleRemoved]);
+
+function handleAdded(added: Element) {
+    for (const banner of added.querySelectorAll<HTMLButtonElement>("button.imgit-youtube-banner"))
+        banner.addEventListener("click", handleBannerClick);
+    for (const poster of added.querySelectorAll<HTMLDivElement>("div.imgit-youtube-poster"))
+        poster.addEventListener("click", handlePlayClick);
+}
+
+function handleRemoved(removed: Element) {
+    for (const banner of removed.querySelectorAll<HTMLButtonElement>("button.imgit-youtube-banner"))
+        banner.removeEventListener("click", handleBannerClick);
+    for (const poster of removed.querySelectorAll<HTMLDivElement>("div.imgit-youtube-poster"))
+        poster.removeEventListener("click", handlePlayClick);
+}
+
+function handleBannerClick(event: Event) {
+    const button = <HTMLButtonElement>event.currentTarget;
+    const href = button.dataset.href!;
+    window.open(href, "_blank");
+}
+
+function handlePlayClick(event: Event) {
+    const poster = <HTMLDivElement>event.currentTarget;
+    const container = <HTMLDivElement>poster.parentElement;
+    const iframe = <HTMLIFrameElement>container.lastChild!.firstChild;
+    container.classList.add("imgit-youtube-loading");
+    iframe.src = iframe.dataset.src!;
+    iframe.addEventListener("load", handlePlayerLoaded);
+}
+
+function handlePlayerLoaded(event: Event) {
+    const iframe = <HTMLIFrameElement>event.currentTarget;
+    const player = <HTMLDivElement>iframe.parentElement;
+    const container = <HTMLDivElement>player.parentElement;
+    iframe.click();
+    container.classList.add("imgit-youtube-playing");
+    player.hidden = false;
+}
--- docs/.vitepress/imgit/plugin/youtube/server.ts
+++ docs/.vitepress/imgit/plugin/youtube/server.ts
@@ -1,5 +1,5 @@
 import { Plugin } from "../../server";
-import { BuiltAsset, ResolvedAsset } from "../../server/asset";
+import { BuiltAsset, ResolvedAsset, AssetSyntax } from "../../server/asset";
 import { Cache, cache as $cache, cfg, std, defaults } from "../../server";
 
 /** YouTube plugin preferences. */
@@ -41,22 +41,34 @@ async function resolve(asset: ResolvedAsset): Promise<boolean> {
 async function build(asset: BuiltAsset): Promise<boolean> {
     if (!isYouTube(asset.syntax.url)) return false;
     const id = getYouTubeId(asset.syntax.url);
-    const source = `https://www.youtube-nocookie.com/embed/${id}`;
-    const title = prefs.title !== false
-        ? `<div class="imgit-youtube-title">${asset.syntax.alt}</div>` : ``;
-    const banner = prefs.banner !== false
-        ? `<button class="imgit-youtube-banner" title="Watch video on YouTube">Watch on</button>` : ``;
+    const source = `https://www.youtube-nocookie.com/embed/${id}?autoplay=1&playsinline=1`;
+    const allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+    const banner = buildBanner(asset.syntax);
+    const title = buildTitle(asset.syntax);
     asset.html = `
 <div class="imgit-youtube" data-imgit-container>${title}${banner}
     <div class="imgit-youtube-poster" title="Play YouTube video">
-        <button class="imgit-youtube-play" title="Play YouTube video"/>
+        <div class="imgit-youtube-play" title="Play YouTube video"/>
         ${await buildPoster(asset)}
     </div>
     <div class="imgit-youtube-player" hidden>
-        <iframe title="${asset.syntax.alt}" data-imgit-src="${source}" allowfullscreen></iframe>
+        <iframe title="${asset.syntax.alt}" data-src="${source}" allow="${allow}" allowfullscreen/>
     </div>
 </div>`;
     return true;
+}
+
+function buildTitle(syntax: AssetSyntax) {
+    if (prefs.title === false || !syntax.alt) return "";
+    const cls = "imgit-youtube-title";
+    return `<div class="${cls}">${syntax.alt}</div>`;
+}
+
+function buildBanner(syntax: AssetSyntax): string {
+    if (prefs.banner === false) return "";
+    const cls = "imgit-youtube-banner";
+    const title = "Watch video on YouTube";
+    return `<button class="${cls}" title="${title}" data-href="${syntax.url}">Watch on</button>`;
 }
 
 async function buildPoster(asset: BuiltAsset): Promise<string> {
