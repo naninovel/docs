40de3d2c5749f599e5489c30c3fe982d6effae57
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-31 05:03:26 +0300

    patch combo

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,2 +1,2 @@
 # https://github.com/vuejs/vitepress/issues/3264
-sed -i -e "s/link.baseURI);/link.baseURI);\nif (link?.href?.startsWith('https:')) return;/g" node_modules/vitepress/dist/client/app/router.js
+sed -i -e "s/link.baseURI);/link.baseURI);\nif (link?.href === 'https:\/\/naninovel.com\/') return;/g" node_modules/vitepress/dist/client/app/router.js
