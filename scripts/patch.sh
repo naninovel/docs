822a384f3a6203150a60c921ec9f8954fcdefe79
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-31 04:15:17 +0300

    patch

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,1 +1,2 @@
+# https://github.com/vuejs/vitepress/issues/3264
 sed -i -e "s/link.baseURI);/link.baseURI);\nif (link?.href?.startsWith('https:')) return;/g" node_modules/vitepress/dist/client/app/router.js
