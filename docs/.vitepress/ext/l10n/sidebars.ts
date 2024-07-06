a7270d335a802534f7de8b68fa1bcc83f776352b
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-07-06 11:56:56 +0300

    update docs

--- docs/.vitepress/ext/l10n/sidebars.ts
+++ docs/.vitepress/ext/l10n/sidebars.ts
@@ -29,7 +29,6 @@ export const en: DefaultTheme.SidebarItem[] = [
             { text: "Script Expressions", link: "/guide/script-expressions" },
             { text: "Managed Text", link: "/guide/managed-text" },
             { text: "Localization", link: "/guide/localization" },
-            { text: "Spreadsheet", link: "/guide/spreadsheet" },
             { text: "Resource Providers", link: "/guide/resource-providers" },
             { text: "Community Modding", link: "/guide/community-modding" },
             { text: "Development Console", link: "/guide/development-console" }
@@ -57,6 +56,7 @@ export const en: DefaultTheme.SidebarItem[] = [
         text: "Extensions",
         collapsed: true,
         items: [
+            { text: "Spreadsheet", link: "/guide/spreadsheet" },
             { text: "Fountain", link: "/guide/fountain" },
             { text: "Playmaker", link: "/guide/playmaker" },
             { text: "Visual Scripting", link: "/guide/visual-scripting" },
@@ -96,7 +96,6 @@ export const js: DefaultTheme.SidebarItem[] = [
             { text: "スクリプト表記", link: "/ja/guide/script-expressions" },
             { text: "テキスト管理", link: "/ja/guide/managed-text" },
             { text: "ローカライズ", link: "/ja/guide/localization" },
-            { text: "Spreadsheet", link: "/ja/guide/spreadsheet" },
             { text: "リソースプロバイダー", link: "/ja/guide/resource-providers" },
             { text: "コミュニティMod", link: "/ja/guide/community-modding" },
             { text: "開発コンソール", link: "/ja/guide/development-console" }
@@ -123,6 +122,7 @@ export const js: DefaultTheme.SidebarItem[] = [
         text: "エクステンション",
         collapsed: true,
         items: [
+            { text: "Spreadsheet", link: "/ja/guide/spreadsheet" },
             { text: "Fountain", link: "/ja/guide/fountain" },
             { text: "Playmaker", link: "/ja/guide/playmaker" },
             { text: "Visual Scripting", link: "/ja/guide/visual-scripting" },
@@ -162,7 +162,6 @@ export const zn: DefaultTheme.SidebarItem[] = [
             { text: "脚本表达式", link: "/zh/guide/script-expressions" },
             { text: "托管文本", link: "/zh/guide/managed-text" },
             { text: "本地化", link: "/zh/guide/localization" },
-            { text: "Spreadsheet", link: "/zh/guide/spreadsheet" },
             { text: "资源加载器", link: "/zh/guide/resource-providers" },
             { text: "社区模组", link: "/zh/guide/community-modding" },
             { text: "开发控制台", link: "/zh/guide/development-console" }
@@ -189,6 +188,7 @@ export const zn: DefaultTheme.SidebarItem[] = [
         text: "扩展",
         collapsed: true,
         items: [
+            { text: "Spreadsheet", link: "/zh/guide/spreadsheet" },
             { text: "Fountain", link: "/zh/guide/fountain" },
             { text: "Playmaker", link: "/zh/guide/playmaker" },
             { text: "Visual Scripting", link: "/zh/guide/visual-scripting" },
@@ -228,7 +228,6 @@ export const ru: DefaultTheme.SidebarItem[] = [
             { text: "Выражения сценария", link: "/ru/guide/script-expressions" },
             { text: "Управляемый текст", link: "/ru/guide/managed-text" },
             { text: "Локализация", link: "/ru/guide/localization" },
-            { text: "Таблицы", link: "/ru/guide/spreadsheet" },
             { text: "Провайдеры ресурсов", link: "/ru/guide/resource-providers" },
             { text: "Моддинг сообщества", link: "/ru/guide/community-modding" },
             { text: "Консоль разработчика", link: "/ru/guide/development-console" }
@@ -255,6 +254,7 @@ export const ru: DefaultTheme.SidebarItem[] = [
         text: "Расширения",
         collapsed: true,
         items: [
+            { text: "Таблицы", link: "/ru/guide/spreadsheet" },
             { text: "Fountain", link: "/ru/guide/fountain" },
             { text: "Playmaker", link: "/ru/guide/playmaker" },
             { text: "Visual Scripting", link: "/ru/guide/visual-scripting" },
